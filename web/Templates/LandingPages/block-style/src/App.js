4977314d3922a31aa4851d36ccdd6f0265687300
cedric-sd <cedricsoares813@gmail.com>
2020-11-28 19:24:24 -0300

    Landing Template: initial structure ⏳

---
+++ web/Templates/LandingPages/block-style/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//componente interno
+import Routes from './routes';
+//style
+import './styles/global.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  )
+}
+
+export default App;
