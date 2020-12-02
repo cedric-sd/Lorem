64a60691dfcff39169c9af71a7912bc9b01d4a43
cedric-sd <cedricsoares813@gmail.com>
2020-12-02 16:53:57 -0300

    added responsivity

--- web/Templates/Goon/actions.js
+++ web/Templates/Goon/actions.js
@@ -2,6 +2,28 @@ $(document).ready(function(){
   $('.carousel-work').slick({
     infinite: true,
     slidesToScroll: 1,
-    slidesToShow: 2.8,
+    slidesToShow: 2.9,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
